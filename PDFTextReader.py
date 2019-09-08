21b9342dedcfeacd53c126558c80ef1fe502da2e
Younes Subhi <yssubhi@gmail.com>
2019-09-08 15:00:49 +0200

    Update PDFTextReader.py

--- PDFTextReader.py
+++ PDFTextReader.py
@@ -6,5 +6,6 @@
 
 from tika import parser
 
+## insert document pathname
 raw = parser.from_file('/Users/younessubhi/Documents/GitHub/Discere/pdf files/deep-image-reconstruction-human-brain.pdf')
 print(raw['content'])
