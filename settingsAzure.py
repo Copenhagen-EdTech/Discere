85ccb812e465f20c1ab8be035aa11662d9dd2e15
Younes Subhi <yssubhi@gmail.com>
2019-09-22 17:32:03 +0200

    texttospeech

---
+++ settingsAzure.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 22 16:09:59 2019
+
+@author: younessubhi
+"""
+
+### settings for azure cognitive services ###
+
+# KimSpeech
+
+key1 = '0eafaa07632e4872b8dba79022e3e27b'
+
+endpoint1 = 'https://northeurope.api.cognitive.microsoft.com/sts/v1.0/issuetoken'
+
+# KimText
+
+key2 = '2e2009db6f474f9e9cdabd2236ef8913'
+
+endpoint2 = 'https://northeurope.api.cognitive.microsoft.com/'
